6d0ca67f2b5c88130905ea7366f09a23ea82746b
roman-wst <tyurin.roman1@gmail.com>
2021-12-13 11:34:25 +0300

    Add props

---
+++ src/components/app/app.js
@@ -0,0 +1,30 @@
+import AppInfo from '../app-info/app-info';
+import SearchPanel from '../search-panel/search-panel';
+import AppFilter from '../app-filter/app-filter';
+import EmployeesList from '../employees-list/employees-list';
+import EmployeesAddForm from '../employees-add-form/employees-add-form';
+
+import './app.css';
+
+function App() {
+  const data = [
+     {name: 'John C.', salary: '800'},
+     {name: 'Alex A.', salary: '2000'},
+     {name: 'Sam S.', salary: '4000'},
+  ];
+  return (
+    <div className="app">
+        <AppInfo />
+
+        <div className="search-panel">
+            <SearchPanel/>
+            <AppFilter/>
+        </div>
+        
+        <EmployeesList data={data}/>
+        <EmployeesAddForm/>
+    </div>
+  );
+}
+
+export default App;
