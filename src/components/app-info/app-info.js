ec6b58336e8c0f1ff0f7921235c5ded86987a948
roman-wst <tyurin.roman1@gmail.com>
2021-12-14 14:18:03 +0300

    Add interactive and event for emploees (like, increase)

--- src/components/app-info/app-info.js
+++ src/components/app-info/app-info.js
@@ -1,11 +1,13 @@
 import "./app-info.css";
 
-const AppInfo = () => {
+const AppInfo = ({countEmpoyees, countEmpoyeesIncreased, countEmpoyeesLiked}) => {
+
     return (
         <div className="app-info">
             <h1>Учет сотрудников в компании N</h1>
-            <h2>Общее число сотрудников:</h2>
-            <h2>Премию получат:</h2>
+            <h2>Общее число сотрудников: {countEmpoyees}</h2>
+            <h2>Премию получат: {countEmpoyeesIncreased}</h2> 
+            <h2>Сотрудников в избранном: {countEmpoyeesLiked}</h2> 
         </div>
     )
 }
--- src/components/app/app.js
+++ src/components/app/app.js
@@ -13,11 +13,12 @@ class App extends Component {
     super(props);
     this.state = {
       data: [
-        {name: 'John C.', salary: '800', increase: false, id: 1},
-        {name: 'Alex A.', salary: '2000', increase: true, id: 2},
-        {name: 'Sam S.', salary: '4000', increase: false, id: 3},
+        {name: 'John C.', salary: '800', increase: false, like: true, id: 1},
+        {name: 'Alex A.', salary: '2000', increase: true, like: false, id: 2},
+        {name: 'Sam S.', salary: '4000', increase: false, like: false, id: 3},
       ]
     }
+    this.maxId = 4;
   }
 
   deleteItem = (id) => {
@@ -32,23 +33,80 @@ class App extends Component {
     })
   }
 
-  render() {
+  addItem = (name, salary) => {
+    const newItem = {
+        name, 
+        salary,
+        increase: false,
+        like: false,
+        id: this.maxId++
+    }
+    this.setState(({data}) => {
+        const newArr = [...data, newItem];
+        return {
+            data: newArr
+        }
+    });
+  }
 
-   return (
-     <div className="app">
-         <AppInfo />
- 
-         <div className="search-panel">
-             <SearchPanel/>
-             <AppFilter/>
-         </div>
-         
-         <EmployeesList
-           data={this.state.data}
-           onDelete={this.deleteItem}
-         />
-         <EmployeesAddForm/>
-     </div>
+  onToggleIncrease = (id) => {
+    // this.setState(({data}) => {
+    //   const index = data.findIndex(elem => elem.id === id);
+
+    //   const old = data[index];
+    //   const newItem = {...old, increase: !old.increase};
+    //   const newArr = [...data.slice(0, index), newItem, ...data.slice(index + 1)];
+
+    //   return {
+    //     data: newArr
+    //   }
+    // })
+    this.setState(({data}) => ({
+      data: data.map(item => {
+        if (item.id === id) {
+          return {...item, increase: !item.increase}
+        } 
+        return item;
+      })
+    }))
+  }
+
+  onToggleProp = (id, prop) => {
+    this.setState(({data}) => ({
+      data: data.map(item => {
+        if (item.id === id) {
+          return {...item, [prop]: !item[prop]}
+        } 
+        return item;
+      })
+    }))
+  }
+
+  render() {
+    const countEmpoyees = this.state.data.length;
+    const countEmpoyeesIncreased = this.state.data.filter(item => item.increase).length;
+    const countEmpoyeesLiked = this.state.data.filter(item => item.like).length;
+
+    return (
+      <div className="app">
+        <AppInfo 
+          countEmpoyees={countEmpoyees}
+          countEmpoyeesIncreased={countEmpoyeesIncreased}
+          countEmpoyeesLiked={countEmpoyeesLiked}
+        />
+
+        <div className="search-panel">
+          <SearchPanel/>
+          <AppFilter/>
+        </div>
+
+        <EmployeesList
+          data={this.state.data}
+          onDelete={this.deleteItem}
+          onToggleProp={this.onToggleProp}
+        />
+        <EmployeesAddForm onAdd={this.addItem} />
+      </div>
    )
   }
 }
